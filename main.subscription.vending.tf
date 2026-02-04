862fbf9fa4577a317481acf9e8f6a882f091c267
shuhei0720 <155175491+shuhei0720@users.noreply.github.com>
2026-02-04 02:45:05 +0000

    テスト用のサブスクリプションを払い出い

--- main.subscription.vending.tf
+++ main.subscription.vending.tf
@@ -37,8 +37,7 @@ resource "azurerm_subscription" "this" {
   for_each = local.subscriptions
 
   subscription_name = each.value.display_name
-  alias             = each.key
-  billing_scope_id  = data.azurerm_billing_mca_account_scope.this[0].id
+  billing_scope_id  = data.azurerm_billing_mca_account_scope.this.id
   workload          = lookup(each.value, "workload_type", "Production")
 
   tags = lookup(each.value, "tags", {})
